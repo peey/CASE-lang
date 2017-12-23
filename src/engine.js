b3330b3a9834eb0a3d7af8a5890aeded8cb5cfb3
Peeyush Kushwaha <peeyush.p97@gmail.com>
2017-12-23 20:32:47 +0530

    Implement Circle, Intersection Logic + tests
    Engine is now completely implemented and tested <3

--- src/engine.js
+++ src/engine.js
@@ -1,7 +1,9 @@
 //TODO: swap out algebra.js for algebraite because algebra.js doesn't support square roots
-import nerdamer from 'nerdamer'
+import nerdamer from 'nerdamer/all'
 import Complex from 'complex.js'
 import h from './helpers.js'
+
+console.log("nerdamer", nerdamer)
 
 export class Point {
   constructor({x, y}) {
@@ -15,17 +17,45 @@ export class Point {
       im: h.val(this.y)
     })
   }
+
+  toString() {
+    return "(" + this.x.toString() + "," + this.y.toString() + ")"
+  }
 }
 
 export class Line {
-  constructor(p1, p2) {
-    if (h.eq(p1.x, p2.x)) {
-      // the line is vertical
-      this.slope = Infinity
-      this.xIntercept = p1.x
+  constructor({points, eqn}) {
+    if (points) {
+      const [p1, p2] = points
+      if (h.eq(p1.x, p2.x)) {
+        // the line is vertical
+        this.slope = Infinity
+        this.xIntercept = p1.x
+      } else {
+        this.slope = p1.y.subtract(p2.y).divide(p1.x.subtract(p2.x))
+        this.yIntercept = p1.y.subtract(this.slope.multiply(p1.x))
+        this.equation = nerdamer("y = mx + c", {m: this.slope, c: this.yIntercept})
+      }
+    } else if (eqn) {
+      this.equation = eqn
+
+      if (eqn.variables().indexOf("y") > -1) {
+        // means slope is finite
+        if (eqn.variables().indexOf("x") > -1) {
+          // means slope is nonzero
+          const coeffs = nerdamer.coeffs(eqn.solveFor("y"), "x")
+          this.slope = nerdamer(nerdamer.vecget(coeffs, 1))
+          this.yIntercept = nerdamer(nerdamer.vecget(coeffs, 0))
+        } else {
+          this.slope = nerdamer(0)
+          this.yIntercept = eqn.solveFor("y")[0]
+        }
+      } else {
+        this.slope = Infinity
+        this.xIntercept = eqn.solveFor("x")[0]
+      }
     } else {
-      this.slope = p1.y.subtract(p2.y).divide(p1.x.subtract(p2.x))
-      this.yIntercept = p1.y.subtract(this.slope.multiply(p1.x))
+      throw "a tantrum"
     }
   }
 
@@ -42,6 +72,91 @@ export class Line {
       return this.slope == Infinity && anotherLine.slope == Infinity && h.eq(this.xIntercept, anotherLine.xIntercept)
     } else {
       return h.eq(anotherLine.slope, this.slope) && h.eq(anotherLine.yIntercept, this.yIntercept)
+    }
+  }
+
+  pointOfIntersection(anotherLine) {
+    const l1 = this, l2 = anotherLine
+
+    if (!(l1.slope == Infinity && l2.slope == Infinity)) {
+      if (l1.slope == Infinity || l2.slope == Infinity) {
+        const l1 = this.slope == Infinity? this : anotherLine
+        const l2 = l1 == this? anotherLine : this
+
+        const x = l1.xIntercept
+        const y = l2.slope.multiply(x).add(l2.yIntercept)
+        return new Point({x, y})
+      } else if (!h.eq(l1.slope, l2.slope)) {
+        const x = l2.yIntercept.subtract(l1.yIntercept).divide(l1.slope.subtract(l2.slope))
+        const y = l1.slope.multiply(x).add(l1.yIntercept)
+        return new Point({x, y})
+      }
+    }
+  }
+}
+
+
+//return values: 0 means equal, -1 means distance is less than d, +1 means distance is more than d
+function distanceCompare(p1, p2, d) {
+  const distanceSquared = nerdamer("(a - b)^2 + (c - d)^2", {a: p1.x, b: p2.x, c: p1.y, d: p2.y})
+  const dSquared = nerdamer(d).pow(2)
+  const comparison = h.val(distanceSquared) - h.val(dSquared)
+  if (comparison == 0) {
+    return 0
+  } else if (comparison < 0) {
+    return -1
+  } else {
+    return 1
+  }
+}
+
+export class Circle {
+  constructor(center, radius) {
+    this.center = center // must be a point
+    this.radius = nerdamer(radius)
+    this.equation = nerdamer("(x - h)^2 + (y - k)^2 = (r)^2", {
+      h: this.center.x,
+      k: this.center.y
+    }).sub("r", this.radius) //FIXME: for some reason I can't directly substitute r in the above call...
+  }
+
+  boundaryPoint(p) {
+    return distanceCompare(this.center, p, this.radius) == 0
+  }
+
+  pointsOfIntersectionWithLine(l) {
+    if (l.slope == Infinity) {
+      const x = l.xIntercept
+      const comparison = distanceCompare(this.center, new Point({x, y: this.center.y}), this.radius)
+      if (comparison == 0) { // then the line is tangent to the circle
+        return [new Point({x, y: this.center.y})]
+      } else if (comparison == -1) {
+        const ySolutions = this.equation.sub("x", x).solveFor("y")
+        return ySolutions.map(y => new Point({x, y: nerdamer(y, {x: x})}))
+      }
+    } else {
+      const eqnInX = this.equation.sub("y", nerdamer("(m*x + c)", {m: l.slope, c: l.yIntercept}))
+      const xSolutions = eqnInX.solveFor("x")
+      return xSolutions.map(x => new Point({x, y: l.equation.sub("x", x).solveFor("y")}))
+    }
+  }
+
+  pointsOfIntersectionWithCircle(c) {
+    const compare = distanceCompare(this.center, c.center, this.radius.add(c.radius))
+
+    if (compare != 1) {
+      //equation of the line containing the two points: https://math.stackexchange.com/a/256123/41484
+      const lineEquation = nerdamer("-2*(x)*(a - b) -2*(y)*(c - d) = (r^2 - s^2) - (a^2 - b^2) - (c^2 - d^2)")
+        .sub("a", this.center.x)
+        .sub("b", c.center.x)
+        .sub("c", this.center.y)
+        .sub("d", c.center.y)
+        .sub("r", this.radius)
+        .sub("s", c.radius)
+
+      const line = new Line({eqn: lineEquation})
+
+      return this.pointsOfIntersectionWithLine(line)
     }
   }
 }
--- test/unit/specs/Engine.spec.js
+++ test/unit/specs/Engine.spec.js
@@ -1,5 +1,5 @@
-import {Point, Line} from '@/engine'
-import nerdamer from "nerdamer"
+import {Point, Line, Circle} from '@/engine'
+import nerdamer from "nerdamer/all"
 import assert from "assert"
 import Complex from "complex.js"
 import h from '@/helpers.js'
@@ -13,31 +13,55 @@ describe('Point', () => {
 })
 
 describe('Line', () => {
-  it("correctly calculates slope and y-intercept", () => {
-    const p1 = new Point({x: 5, y: 2})
-    const p2 = new Point({x: 0, y: 0})
+  describe('Instantiation', () => {
 
-    const line = new Line(p1, p2)
+    it("correctly calculates slope and y-intercept", () => {
+      const p1 = new Point({x: 5, y: 2})
+      const p2 = new Point({x: 0, y: 0})
 
-    assert(h.eq(line.slope, nerdamer("2/5")))
-    assert(h.eq(line.yIntercept, 0))
-  }),
-  it("handles the case of vertical lines", () => {
-    const p1 = new Point({x: 3, y: 3})
-    const p2 = new Point({x: 3, y: 6})
+      const line = new Line({points: [p1, p2]})
 
-    const line = new Line(p1, p2)
+      assert(h.eq(line.slope, nerdamer("2/5")))
+      assert(h.eq(line.yIntercept, 0))
+    })
 
-    assert(line.slope == Infinity)
-    assert(h.eq(line.xIntercept, 3))
-  }),
+    it("handles the case of vertical lines", () => {
+      const p1 = new Point({x: 3, y: 3})
+      const p2 = new Point({x: 3, y: 6})
+
+      const line = new Line({points: [p1, p2]})
+
+      assert(line.slope == Infinity)
+      assert(h.eq(line.xIntercept, 3))
+    })
+
+    it("correctly instantiates from an equation", () => {
+      const eqn1 = nerdamer("3y + 5x - 8 = 0")
+      const eqn2 = nerdamer("9x = sqrt(8)")
+      const eqn3 = nerdamer("9y = sqrt(8)")
+
+      const line1 = new Line({eqn: eqn1})
+      const line2 = new Line({eqn: eqn2})
+      const line3 = new Line({eqn: eqn3})
+
+      assert(h.eq(line1.slope, nerdamer("-5/3")))
+      assert(h.eq(line1.yIntercept, nerdamer("8/3")))
+
+      assert(line2.slope == Infinity)
+      assert(h.eq(nerdamer(line2.xIntercept), nerdamer("sqrt(8)/9")))
+
+      assert(h.eq(line3.slope, 0))
+      assert(h.eq(line3.yIntercept, nerdamer("sqrt(8)/9")))
+    })
+  })
+
   it("correctly tells if it contains a point or not", () => {
     const p1 = new Point({x: 5, y: 2})
     const p2 = new Point({x: 0, y: 0})
     const p3 = new Point({x: 5, y: 10})
 
-    const line1 = new Line(p1, p2)
-    const line2 = new Line(p1, p3)
+    const line1 = new Line({points: [p1, p2]})
+    const line2 = new Line({points: [p1, p3]})
 
     assert(line1.containsPoint(new Point({x: 10, y: 4})))
     assert(!line1.containsPoint(new Point({x: 10, y: 3})))
@@ -46,7 +70,8 @@ describe('Line', () => {
     assert(line2.containsPoint(new Point({x: 5, y: 4})))
     assert(!line2.containsPoint(new Point({x: 6, y: 4})))
     assert(line2.containsPoint(new Point({x: 5, y: nerdamer("sqrt(3)")})))
-  }),
+  })
+
   it("can tell if another line is same as this one", () => {
     const p1 = new Point({x: 0, y: 1})
     const p2 = new Point({x: 5, y: 3})
@@ -54,11 +79,11 @@ describe('Line', () => {
     const p4 = new Point({x: nerdamer("5*sqrt(3)"), y: 0})
     const p5 = new Point({x: nerdamer("5*sqrt(3)"), y: 100})
 
-    const line1 = new Line(p1, p2)
-    const line2 = new Line(p1, p3)
+    const line1 = new Line({points: [p1, p2]})
+    const line2 = new Line({points: [p1, p3]})
 
-    const line3 = new Line(p3, p4)
-    const line4 = new Line(p3, p5)
+    const line3 = new Line({points: [p3, p4]})
+    const line4 = new Line({points: [p3, p5]})
 
     assert(line1.same(line2))
     assert(line2.same(line1))
@@ -67,5 +92,97 @@ describe('Line', () => {
     assert(line4.same(line3))
 
     assert(!line1.same(line3))
+  })
+
+  it("can give point of intersection of two non parallel lines", () => {
+    const line1 = new Line({eqn: nerdamer("3x + 5y = 7")})
+    const line2 = new Line({eqn: nerdamer("6x + 5y = 7")})
+    const line3 = new Line({eqn: nerdamer("12x + 10y = 14")})
+    const line4 = new Line({eqn: nerdamer("12x + 10y = 15")})
+    const line5 = new Line({eqn: nerdamer("12x = 5")})
+
+    //oblique
+    const p1 = line1.pointOfIntersection(line2)
+    assert(h.eq(p1.x, 0))
+    assert(h.eq(p1.y, nerdamer("7/5")))
+
+    //same
+    const p2 = line2.pointOfIntersection(line3)
+    assert(typeof p2 == "undefined")
+
+    //parallel
+    const p3 = line3.pointOfIntersection(line4)
+    assert(typeof p3 == "undefined")
+
+    //one of them vertical
+    const p4 = line4.pointOfIntersection(line5)
+    assert(h.eq(p4.x, nerdamer("5/12")))
+    assert(h.eq(p4.y, nerdamer("1")))
+
+    //check for commutavitiy
+    const p5 = line5.pointOfIntersection(line4)
+    assert(h.eq(p5.x, nerdamer("5/12")))
+    assert(h.eq(p5.y, nerdamer("1")))
+  })
+})
+
+describe("Circle", () => {
+  it("constructs properly", () => {
+    const c = new Circle(new Point({x: 3, y: 6}), nerdamer("sqrt(45)"))
+    assert(h.eq(c.center.x, 3)) // got emperically
+    assert(h.eq(c.center.y, 6)) // got emperically
+    assert(h.eq(c.radius, nerdamer("sqrt(45)"))) // got emperically
+  })
+
+  it("can make a correct equation", () => {
+    const c = new Circle(new Point({x: 3, y: 6}), nerdamer("sqrt(45)"))
+    assert(c.equation.toString() == "-12*y+y^2-6*x+x^2+45=45") // got emperically
+  })
+
+  it("can tell if a point is a boundary point", () => {
+    const c = new Circle(new Point({x: 3, y: 6}), nerdamer("sqrt(45)"))
+
+    assert(c.boundaryPoint(new Point({x: 0, y: 0})))
+    assert(c.boundaryPoint(new Point({x: nerdamer("3 + sqrt(45)"), y: 6})))
+    assert(!c.boundaryPoint(new Point({x: 1, y: 1})))
+  })
+
+  it("can find points of intersection with a line", () => {
+    const c = new Circle(new Point({x: 3, y: 6}), nerdamer("sqrt(45)"))
+
+
+    let l, results, yvals
+    // vertical line
+    l = new Line({eqn: nerdamer("x = 0")})
+    results = c.pointsOfIntersectionWithLine(l)
+    assert(h.eq(results[0].x, 0))
+    assert(h.eq(results[1].x, 0))
+
+    // don't know how to sort nerdamer values for now
+    assert(h.eq(results[0].y, 0) && h.eq(results[1].y, 12) || h.eq(results[1].y, 0) && h.eq(results[0].y, 12))
+
+    // random line
+    l = new Line({eqn: nerdamer("y = x")})
+    results = c.pointsOfIntersectionWithLine(l)
+    // don't know how to sort nerdamer values for now
+    assert(h.eq(results[0].x, 0) && h.eq(results[1].x, 9) || h.eq(results[0].x, 9) && h.eq(results[1].x, 0))
+    assert(h.eq(results[0].y, 0) && h.eq(results[1].y, 9) || h.eq(results[0].y, 9) && h.eq(results[1].y, 0))
+
+    // tangent
+    l = new Line({eqn: nerdamer("y = 6 - sqrt(45)")})
+    results = c.pointsOfIntersectionWithLine(l)
+    assert(results.length == 1)
+    assert(h.eq(results[0].x, "3") && h.eq(results[0].y, nerdamer("6 - sqrt(45)")))
+  })
+
+  it("can find points of intersection with another circle", () => {
+    const c1 = new Circle(new Point({x:2, y:1}), 5)
+    const c2 = new Circle(new Point({x:10, y:1}), 7)
+    const results = c1.pointsOfIntersectionWithCircle(c2)
+    // verified by wolfaram alpha : https://www.wolframalpha.com/input/?i=intersection+points+of+(x+-+2)%5E2+%2B+(y-1)%5E2+%3D+25,+(x+-+10)%5E2+%2B+(y-1)%5E2+%3D+49
+    assert(h.eq(results[0].x, nerdamer("9/2")) && h.eq(results[0].x, nerdamer("9/2")))
+    const a = nerdamer("(1/2)*(5*sqrt(3)+2)")
+    const b = nerdamer("(1/2)*(-5*sqrt(3)+2)")
+    assert(h.eq(results[0].y, a) && h.eq(results[1].y, b) || h.eq(results[0].y, b) && h.eq(results[1].y, a))
   })
 })
